2c6b5c183bded1277ba8dc327ae1f989a1d3f0c1
DataMagus <neiyercorreal@hotmail.com>
2014-08-21 21:56:53 -0400

    Initial Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 for R Programming Class
 
 ## Write a short comment describing this function
-
+## Creates a CacheMatrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function (inverse) inv<<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, 
+         setinv = setinv,
+         getinv = getinv)    
 }
 
 
 ## Write a short comment describing this function
-
+## Returns the inverse matrix.
+## Returns the cashed inverse if available
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv<- solve(data,...)
+    x$setinv(inv)
+    inv
 }
